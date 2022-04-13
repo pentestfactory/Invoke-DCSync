9300d9a8a68324667f51a7c2a7bd4d33e7441670
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2022-04-13 15:47:42 +0200

    Update Invoke-DCSync.ps1

--- Invoke-DCSync.ps1
+++ Invoke-DCSync.ps1
@@ -119,7 +119,7 @@ if ($confirmation -eq 'y') {
 
     # final message
     Write-Host ""
-    Write-Host "[OK] Extraction completed [" $csv_obj.length "users ]" -ForegroundColor Green
+    Write-Host "[OK] Extraction completed for" $csv_obj.length "user accounts" -ForegroundColor Green
     Write-Host "  > Please submit the 'PTF' directory to Pentest Factory GmbH" -ForegroundColor Gray
     Write-Host "  > Please consider all files as confidential!" -ForegroundColor Gray
     Write-Host ""
